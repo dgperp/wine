2b8704e210ca94bae2c6a8cb1d91c16048ed0dea
Mr. Q <yuthajakyalom@gmail.com>
2025-03-11 11:37:06 +0700

    [add] add odoolint

--- update_odoo.sh
+++ update_odoo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MODULES=${MODULES:-}
-DATABASES=$(sudo docker exec -i db psql -U odoo -d postgres -t -c "SELECT string_agg(datname, ',') FROM pg_database WHERE datistemplate = false AND datname != 'postgres';")
+DATABASES=$(sudo docker exec -i db_wine psql -U odoo -d postgres -t -c "SELECT string_agg(datname, ',') FROM pg_database WHERE datistemplate = false AND datname != 'postgres';")
 DATABASES=$(echo $DATABASES | tr -d ' \n' | sed 's/^,//;s/,$//')
 echo "Available databases: $DATABASES"
 sudo docker-compose stop

d520f950b4d1e8f775f9601ec1cdc9c568771ebc
mintchannan <phonphimon.ch.55@ubu.ac.th>
2025-03-20 11:16:24 +0700

    [ADD] module product_class, product_brand, ecommerce_ext

---
+++ addons/product_class/models/product_class.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class ProductBrand(models.Model):
+    _inherit = 'product.template'
+
+    class_id = fields.Many2one('product.class', string='Class', ondelete='restrict')
+    sub_class_id = fields.Many2one('product.sub.class', string='Sub-Class', ondelete='restrict')
+
+
+class ClassProduct(models.Model):
+    _name = 'product.class'
+
+    name = fields.Char(String="Name", required=True)
+    product_ids = fields.One2many('product.template', 'class_id')
+    sub_class_ids = fields.One2many('product.sub.class', 'class_id')
+    description = fields.Text(string='Description')
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', "Class name already exists !"),
+    ]
+
+
+class SubClassProduct(models.Model):
+    _name = 'product.sub.class'
+
+    name = fields.Char(String="Name", required=True)
+    class_id = fields.Many2one('product.class', string='Class', ondelete='restrict')
+    product_ids = fields.One2many('product.template', 'sub_class_id')
+    description = fields.Text(string='Description')
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', "Sub-Class name already exists !"),
+    ]
+
